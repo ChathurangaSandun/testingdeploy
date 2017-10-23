f59ab0d7d292b62793191408eefcec3227779b11
Chathuranga.Sandun <Chathurangasandunkumara@gmail.com>
2017-10-24 00:44:13 +0530

    app insights

--- testingdeploy/Controllers/HomeController.cs
+++ testingdeploy/Controllers/HomeController.cs
@@ -6,24 +6,30 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.ApplicationInsights;
 
 namespace testingdeploy.Controllers
 {
     public class HomeController : Controller
     {
+        TelemetryClient _log = new Microsoft.ApplicationInsights.TelemetryClient();
+
 
         //[HttpPost]
         public ActionResult PunchoutSetupRequest()
         {
 
-            FileStream fs = new FileStream(@"\out.txt", FileMode.Open, FileAccess.ReadWrite);
-            using (StreamReader sr = new StreamReader(fs))
-            {
-                using (StreamWriter sw = new StreamWriter(fs))
-                {
-                    sw.WriteLine(DateTime.Now.ToString(CultureInfo.InvariantCulture));
-                }
-            }
+            _log.TrackTrace("Ok " + DateTime.Now.ToString());
+
+            //FileStream fs = new FileStream(@"\out.txt", FileMode.Open, FileAccess.ReadWrite);
+            //using (StreamReader sr = new StreamReader(fs))
+            //{
+            //    using (StreamWriter sw = new StreamWriter(fs))
+            //    {
+            //        sw.WriteLine(DateTime.Now.ToString(CultureInfo.InvariantCulture));
+            //    }
+            //}
+
 
 
 

9df8cf6255c43b84fbec46a495a438ed4a5fdac8
Chathuranga.Sandun <Chathurangasandunkumara@gmail.com>
2017-10-24 01:23:15 +0530

    set punchout response

--- testingdeploy/Controllers/HomeController.cs
+++ testingdeploy/Controllers/HomeController.cs
@@ -6,7 +6,9 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Serialization;
 using Microsoft.ApplicationInsights;
+using testingdeploy.Models;
 
 namespace testingdeploy.Controllers
 {
@@ -27,12 +29,43 @@ namespace testingdeploy.Controllers
 
             _log.TrackTrace(xml.ToString());
 
+            var xmlSerializer = new XmlSerializer(typeof(MainRequest));
+
+            MainRequest punchOutSetupRequest = (MainRequest)xmlSerializer.Deserialize(new StringReader(xml.ToString()));
 
 
 
+            MainResponse mainResponse = new MainResponse()
+            {
+                PayloadID = punchOutSetupRequest.PayloadID,
+                Timestamp = punchOutSetupRequest.Timestamp,
+                Response = new Response()
+                {
+                    Status = new Status()
+                    {
+                        Code = "200",
+                        Text = "Ok"
+                    },
+                    PunchOutSetupResponse = new PunchOutSetupResponse()
+                    {
+                        StartPage = new StartPage()
+                        {
+                            URL = "https://kumara.azurewebsites.net/home/index"
+                        }
+                    }
 
+                }
+            };
 
-            return View();
+            var stringwriter = new StringWriter();
+
+            XmlSerializer serializer = new XmlSerializer(typeof(MainResponse));
+            serializer.Serialize(stringwriter, mainResponse);
+            var responseTest = stringwriter.ToString().Replace("utf-16","utf-8");
+
+            _log.TrackTrace(responseTest);
+
+            return Content(responseTest);
         }
 
 

d5ba708f05eb8c24a451e779c28ae2a9f9a2e60a
chathuranga sandun <chathuranga.s@eyepax.com>
2018-02-09 16:54:03 +0530

    change post

--- testingdeploy/Controllers/HomeController.cs
+++ testingdeploy/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace testingdeploy.Controllers
         TelemetryClient _log = new Microsoft.ApplicationInsights.TelemetryClient();
         private static MainRequest request;
 
-        [HttpDelete]
+        [HttpPost]
         public ActionResult PunchoutSetupRequest()
         {
 

4c51bd4e0f64ac7838e396cd6b42e51cf99fccbd
chathuranga sandun <chathuranga.s@eyepax.com>
2018-02-16 10:58:57 +0530

    change

--- testingdeploy/Controllers/PunchOutOciController.cs
+++ testingdeploy/Controllers/PunchOutOciController.cs
@@ -122,29 +122,61 @@ namespace testingdeploy.Controllers
                     i++;
                 }
             
-
+            this._log.TrackTrace(d);
             byte[] data = encoding.GetBytes(d);
       
    
 
-            PostDataOfItem(data);
+            PostDataOfItem(d);
 
 
             return this.View();
         }
 
-        private void PostDataOfItem(byte[] data)
+        private void PostDataOfItem(string d)
         {
-            
+            // Create a request using a URL that can receive a post.   
+            WebRequest request = WebRequest.Create(_hookUrl);
+            // Set the Method property of the request to POST.  
+            request.Method = "POST";
+            // Create POST data and convert it to a byte array.  
+            string postData = d;
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            // Set the ContentType property of the WebRequest.  
+            request.ContentType = "application/x-www-form-urlencoded";
+            // Set the ContentLength property of the WebRequest.  
+            request.ContentLength = byteArray.Length;
+            // Get the request stream.  
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.  
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.  
+            dataStream.Close();
+            // Get the response.  
+            WebResponse response = request.GetResponse();
+            // Display the status.  
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.  
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.  
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.  
+            Console.WriteLine(responseFromServer);
+            // Clean up the streams.  
+            reader.Close();
+            dataStream.Close();
+            response.Close();
 
-            HttpWebRequest myRequest =
-                (HttpWebRequest)WebRequest.Create(_hookUrl);
-            myRequest.Method = "POST";
-            myRequest.ContentType = "application/x-www-form-urlencoded";
-            myRequest.ContentLength = data.Length;
-            Stream newStream = myRequest.GetRequestStream();
-            newStream.Write(data, 0, data.Length);
-            newStream.Close();
+            //HttpWebRequest myRequest =
+            //    (HttpWebRequest)WebRequest.Create(_hookUrl);
+            //myRequest.Method = "POST";
+            //myRequest.ContentType = "application/x-www-form-urlencoded";
+            //myRequest.ContentLength = data.Length;
+            //Stream newStream = myRequest.GetRequestStream();
+            //newStream.Write(data, 0, data.Length);
+            //newStream.Close();
         }
     }
 }
